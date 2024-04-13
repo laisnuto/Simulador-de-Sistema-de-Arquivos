05df4ebb5f3fd64a24c8bfcc42954171670bc1ff
Lais_Nuto_Rossman <laisnuto@gmail.com>
2024-04-13 01:55:16 -0300

    inicia ep2

---
+++ ep2.c
@@ -0,0 +1,15 @@
+#include "ep2.h"
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Uso: %s <escalonador> <arquivo_entrada> <arquivo_saida>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+   
+
+    return EXIT_SUCCESS;
+}
+   
